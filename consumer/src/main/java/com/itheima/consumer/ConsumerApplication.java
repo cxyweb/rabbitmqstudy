3b64a678fd346950f7837e8e12038e5a840375e9
cxyweb <2535228845@qq.com>
2024-03-02 23:22:18 +0800

    rabbitmq学习代码

---
+++ consumer/src/main/java/com/itheima/consumer/ConsumerApplication.java
@@ -0,0 +1,21 @@
+package com.itheima.consumer;
+
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.amqp.support.converter.MessageConverter;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class ConsumerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ConsumerApplication.class, args);
+    }
+
+    @Bean
+    public MessageConverter jacksonMessageConvertor(){
+        Jackson2JsonMessageConverter jackson2JsonMessageConverter = new Jackson2JsonMessageConverter();
+        jackson2JsonMessageConverter.setCreateMessageIds(true);
+        return jackson2JsonMessageConverter;
+    }
+}
